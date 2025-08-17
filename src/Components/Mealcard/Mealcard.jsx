5be24415720a8fa971207a549e664cbdc68a2d34
elkashab <elkashab@gmail.com>
2025-08-17 10:36:21 +0300

    Second Commit

--- src/Components/Mealcard/Mealcard.jsx
+++ src/Components/Mealcard/Mealcard.jsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
-function Mealcard(props) {
+function Mealcard({ meal }) {
   async function getMealDetails(idMeal) {
     const { data } = await axios.get(
       "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + idMeal
@@ -14,23 +14,26 @@ function Mealcard(props) {
   }
 
   const [country, setcountry] = useState(null);
-  useEffect(function () {
-    getMealDetails(props.country);
-  }, []);
+  useEffect(
+    function () {
+      getMealDetails(meal.idMeal);
+    },
+    [meal.idMeal]
+  );
   return (
     <div className=" bg-white rounded-4xl flex flex-col items-center mt-15 hover:scale-105 text-center justify-between hover:duration-300">
       <img
-        src={props.meal.strMealThumb}
+        src={meal.strMealThumb}
         alt="meal"
         className="w-[50%] rounded-full hover:rotate-[360deg] duration-700 relative -top-6"
       />
-      <h6 className="text-lg mt-3 font-bold">{props.meal.strMeal}</h6>
+      <h6 className="text-lg mt-3 font-bold">{meal.strMeal}</h6>
       <div className="flex justify-between items-center mt-4 w-[40%]">
         <FontAwesomeIcon icon={solidIcons.faGlobe} />
         <span> {country}</span>
       </div>
       <button className="bg-green-700 px-3 py-2 my-3 text-white rounded-2xl">
-        <Link to={`mealdetails/${props.meal.idMeal}`}>View Receipe</Link>
+        <Link to={`mealdetails/${meal.idMeal}`}>View Receipe</Link>
       </button>
     </div>
   );
